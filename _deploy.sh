10b850e3a0e3ab9f8935157e52d2bf3b8692b32e
zachbogart <zlbogart@gmail.com>
2018-07-19 18:53:21 -0400

    travis

--- _deploy.sh
+++ _deploy.sh
@@ -1,6 +1,6 @@
 # configure your name and email if you have not done so
 git config --global user.email "zb2223@columbia.edu"
-git config --global user.name "Zach Bogart"
+git config --global user.name "jtr13"
 
 # clone the repository to the book-output directory
 git clone -b master \
