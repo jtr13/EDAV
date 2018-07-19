dd882170b56bece57dd605fb03e5fb33e9c1a153
zachbogart <zlbogart@gmail.com>
2018-07-19 17:37:24 -0400

    test travis setup

---
+++ _deploy.sh
@@ -0,0 +1,14 @@
+# configure your name and email if you have not done so
+git config --global user.email "zb2223@columbia.edu"
+git config --global user.name "Zach Bogart"
+
+# clone the repository to the book-output directory
+git clone -b gh-pages \
+  https://${GITHUB_PAT}@github.com/${TRAVIS_REPO_SLUG}.git \
+  book-output
+cd book-output
+git rm -rf *
+cp -r ../_book/* ./
+git add --all *
+git commit -m "Update the book thru Travis"
+git push -q origin gh-pages
