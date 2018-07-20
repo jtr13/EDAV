3fd7fd333dc4adf85bec0f36d98a3e5b285a1a2e
zachbogart <zlbogart@gmail.com>
2018-07-19 23:52:41 -0400

    travis another try

--- _deploy.sh
+++ _deploy.sh
@@ -7,10 +7,10 @@ git clone -b gh-pages \
   https://${GITHUB_PAT}@github.com/${TRAVIS_REPO_SLUG}.git \
   book-output
 cd book-output
-git rm -rf *
-cp -r ../docs/* ./
-mv * docs
+git rm -R docs
+mkdir docs
+cp -r ../docs/* ./docs
 rm -rf docs/*.md docs/CNAME docs/search_index.json 
-git add docs/*
+git add docs
 git commit -m "Update the book thru Travis"
 git push -q origin gh-pages
