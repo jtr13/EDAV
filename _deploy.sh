aad1530af6defbcf9a9187270d77c81d7b15c86a
zachbogart <zlbogart@gmail.com>
2018-07-19 23:15:06 -0400

    travis in gh-pages branch

--- _deploy.sh
+++ _deploy.sh
@@ -10,6 +10,7 @@ cd book-output
 git rm -rf *
 cp -r ../docs/* ./
 mv * docs
-git add --all *
+rm -rf docs/*.md docs/CNAME docs/search_index.json 
+git add docs/*
 git commit -m "Update the book thru Travis"
 git push -q origin gh-pages
