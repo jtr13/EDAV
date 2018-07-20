67f28b25341aaf44217296757562e9ebfe1fcb40
zachbogart <zlbogart@gmail.com>
2018-07-20 13:35:40 -0400

    try joyce howto yml

--- _deploy.sh
+++ _deploy.sh
@@ -1,16 +1,18 @@
-# configure your name and email if you have not done so
+#!/bin/sh
+
+set -e
+
+[ -z "${GITHUB_PAT}" ] && exit 0
+[ "${TRAVIS_BRANCH}" != "master" ] && exit 0
+
 git config --global user.email "zb2223@columbia.edu"
 git config --global user.name "Zach Bogart"
 
-# clone the repository to the book-output directory
-git clone -b gh-pages \
-  https://${GITHUB_PAT}@github.com/${TRAVIS_REPO_SLUG}.git \
-  book-output
+git clone -b gh-pages https://${GITHUB_PAT}@github.com/${TRAVIS_REPO_SLUG}.git book-output
 cd book-output
-git rm -rf *
 cp -r ../_book/* ./
 git add --all *
-git commit -m"Update the book"
+git commit -m"Update the book" || true
 git push -q origin gh-pages
 
 
